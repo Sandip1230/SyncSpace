0468a2beb58a11d1b66efa259f8bdc85f194fbf6
Sandip1230 <maitrasandip99@gmail.com>
2026-07-18 12:16:25 +0530

    Major Room Handling changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // binds 0.0.0.0 (all interfaces, both IPv4 and IPv6)
+    port: 5173,
+  },
+});
--- sockets/roomHandler.js
+++ sockets/roomHandler.js
@@ -1,4 +1,7 @@
-socket.on("room:join", async ({ roomId, username }) => {
+const { getOrCreateRoom, getRoom, listUsers, scheduleEvictionIfEmpty } = require("./roomStore");
+
+function roomHandler(io, socket) {
+  socket.on("room:join", async ({ roomId, username }) => {
     if (!roomId) return;
     socket.join(roomId);
     socket.data.roomId = roomId;
@@ -10,3 +13,23 @@ socket.on("room:join", async ({ roomId, username }) => {
     socket.emit("room:users", listUsers(roomId));
     socket.to(roomId).emit("room:user-joined", { socketId: socket.id, username: socket.data.username });
   });
+
+  socket.on("room:leave", () => leaveCurrentRoom(socket));
+  socket.on("disconnect", () => leaveCurrentRoom(socket));
+}
+
+function leaveCurrentRoom(socket) {
+  const roomId = socket.data.roomId;
+  if (!roomId) return;
+
+  const room = getRoom(roomId);
+  if (room) {
+    room.users.delete(socket.id);
+    socket.to(roomId).emit("room:user-left", { socketId: socket.id });
+    scheduleEvictionIfEmpty(roomId);
+  }
+  socket.leave(roomId);
+  socket.data.roomId = null;
+}
+
+module.exports = roomHandler;
